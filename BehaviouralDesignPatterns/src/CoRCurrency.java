6915b5df465691e7797dda4599db8a0ad40a7afa
gangaramesh08 <gangaramesh08@gmail.com>
2020-05-02 15:37:37 +0530

    Chain of Responsibility Design Pattern - ATM

---
+++ BehaviouralDesignPatterns/src/CoRCurrency.java
@@ -0,0 +1,18 @@
+public class CoRCurrency {
+    int amount;
+
+    public CoRCurrency(int amount) {
+        this.amount = amount;
+    }
+
+    public CoRCurrency() {
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
---
+++ BehaviouralDesignPatterns/src/CoRDispense.java
@@ -0,0 +1,3 @@
+public interface CoRDispense {
+    public void dispense(CoRCurrency currency);
+}
