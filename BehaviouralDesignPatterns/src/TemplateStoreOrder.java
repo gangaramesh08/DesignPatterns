f87844adf09ffc8db0a4cb829e62b1c344b1eccf
gangaramesh08 <gangaramesh08@gmail.com>
2020-05-07 10:13:03 +0530

    Template Design Pattern - OrderProcess

---
+++ BehaviouralDesignPatterns/src/TemplateStoreOrder.java
@@ -0,0 +1,16 @@
+public class TemplateStoreOrder extends TemplateOrder {
+    @Override
+    public void doSelect() {
+        System.out.println("Customer choses the item from the shelf");
+    }
+
+    @Override
+    public void doPayment() {
+        System.out.println("Payment is done at the cash counter");
+    }
+
+    @Override
+    public void doDelivery() {
+        System.out.println("Item is delivered at the collection counter in the store");
+    }
+}
